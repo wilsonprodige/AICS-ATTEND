881c984fe1f01eed966eed776451d893bcd09e04
wilsonprodige <wilsonzeugang@gmail.com>
2023-09-22 09:51:43 +0100

    .env update

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize("aics_attend","root","",{
-    host:'127.0.0.1',
+const dbname=process.env.DB_NAME;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const sequelize = new Sequelize(dbname,username,password,{
+    host:host,
     dialect:'mysql',
     operatorAliases:false
 })
