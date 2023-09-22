aece23993eaa25bc2224eb2b2cf324468b707ef2
wilsonprodige <wilsonzeugang@gmail.com>
2023-09-22 12:52:05 +0100

    fixing frontend request

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL:"http://localhost:8000/api",
+    baseURL:"https://aics-attend.onrender.com/api",
     withCredentials:true,
 })
